8b2e9998232d30a6154e545111eb4d52b7fe540a
Jose Luis Salas <josacar@users.noreply.github.com>
2024-07-29 13:52:59 +0200

    Support ugly command line options

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,50 @@
 use rust_kms::KmsEncryption;
-use std::path::Path;
+use std::{path::Path,env};
 
 #[tokio::main]
 async fn main() {
     let kms_encryption = KmsEncryption::new().await;
-    let key_id = "a5807bb6-cde2-41f4-983d-f5f2f270170f";  // Replace with your AWS KMS key ID
+    let args: Vec<String> = env::args().collect();
 
-    let file_path = Path::new("file.txt");
+    match args.len() {
+        3 => {
+            let operation = &args[1];
+            let file_path = &args[2];
+            let path = Path::new(file_path);
 
-    // Encrypt the file
-    match kms_encryption.encrypt_file(key_id, file_path).await {
-        Ok(_) => eprintln!("File encrypted successfully"),
-        Err(e) => eprintln!("Failed to encrypt file: {:?}", e),
-    }
+            match &operation[..] {
+                "decrypt" => {
+                    match kms_encryption.decrypt_file(path).await {
+                        Ok(_) => eprintln!("File decrypted successfully"),
+                        Err(e) => eprintln!("Failed to decrypt file: {:?}", e),
+                    }
+                },
+                _ => {
+                    eprintln!("Operation not supported");
+                }
+            }
+        },
+        4 => {
+            let operation = &args[1];
+            let file_path = &args[2];
+            let key_id = &args[3];
+            let path = Path::new(file_path);
 
-    println!("\n");
+            match &operation[..] {
+                "encrypt" => {
+                    match kms_encryption.encrypt_file(key_id, path).await {
+                        Ok(_) => eprintln!("File encrypted successfully"),
+                        Err(e) => eprintln!("Failed to encrypt file: {:?}", e),
+                    }
+                },
+                _ => {
+                    eprintln!("Operation not supported");
+                }
+            }
+        },
+        _ => {
+            eprintln!("Wrong combination of arguments");
+        }
 
-    let file_path = Path::new("file.encrypted");
-
-    // Decrypt the file
-    match kms_encryption.decrypt_file(file_path).await {
-        Ok(_) => eprintln!("File decrypted successfully"),
-        Err(e) => eprintln!("Failed to decrypt file: {:?}", e),
     }
 }
